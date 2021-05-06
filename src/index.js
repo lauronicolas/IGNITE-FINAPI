f4120a35a32df815b45216b5ef98e07c3651afbe
lauronicolas <lauro.nicolas.mazotti@gmail.com>
2021-05-05 23:04:54 -0300

    Inicio do projeto

---
+++ src/index.js
@@ -0,0 +1,155 @@
+const { response, request } = require('express');
+const express = require('express');
+
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+//Middleware
+
+function verifyIfExistAccountCPF(request, response, next) {
+      const { cpf } = request.headers;
+
+      const customer = customers.find(customer => customer.cpf === cpf);
+
+      if (!customer) {
+            return response.status(400).json({ message: "Customer not found" });
+      }
+
+      request.customer = customer;
+
+      return next();
+}
+
+function getBalance(statement) {
+      //pega as informações de determinado valor e transforma todos os valores em somente um valor
+      const balance = statement.reduce((acc, operation) => {
+            if (operation.type === 'credit') {
+                  return acc + operation.amount;
+            } else {
+                  return acc - operation.amount;
+            }
+      }, 0);
+
+      return balance;
+}
+
+//Metodo Criar a cota
+app.post("/account", (request, response) => {
+      const { cpf, name } = request.body;
+
+      const customerAlreadyExists = customers.some(
+            (customer) => customer.cpf === cpf
+      );
+
+      if (customerAlreadyExists) {
+            return response.status(400).json({ message: "Customer already exists" });
+      }
+
+
+      customers.push({
+            cpf,
+            name,
+            id: uuidv4(),
+            statement: []
+      });
+
+      return response.status(201).send();
+});
+
+// app.use(verifyIfExistAccountCPF);
+
+app.get("/statement", verifyIfExistAccountCPF, (request, response) => {
+
+      const { customer } = request;
+
+      return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistAccountCPF, (request, response) => {
+      const { description, amount } = request.body;
+
+      const { customer } = request;
+
+      const statementOperation = {
+            description,
+            amount,
+            created_at: new Date(),
+            type: "credit"
+      }
+
+      customer.statement.push(statementOperation);
+
+      return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistAccountCPF, (request, response) => {
+      const { amount } = request.body;
+      const { customer } = request;
+
+      const balance = getBalance(customer.statement);
+
+      if (balance < amount) {
+            return response.status(400).json({ error: "Insufficient funds!" });
+      }
+
+      const statementOperation = {
+            amount,
+            created_at: new Date(),
+            type: "debit"
+
+      }
+
+      customer.statement.push(statementOperation);
+
+      return response.status(201).send();
+})
+
+app.get("/statement/date", verifyIfExistAccountCPF, (request, response) => {
+      const { customer } = request;
+      const { date } = request.query;
+
+      const dateFormat = new Date(date + " 00:00");
+
+      const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString());
+
+      return response.json(statement);
+});
+
+app.put("/account", verifyIfExistAccountCPF, (request, response) => {
+      const { name } = request.body;
+      const { customer } = request;
+
+      customer.name = name;
+
+      return response.status(201).send();
+});
+
+app.get("/account", verifyIfExistAccountCPF, (request, response) => {
+      const { customer } = request;
+
+      return response.json(customer);
+});
+
+app.delete("/account", verifyIfExistAccountCPF, (request, response) => {
+      const { customer } = request;
+
+      //splice
+      customers.splice(customer, 1);
+
+      return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExistAccountCPF, (request, response) => {
+      const { customer } = request;
+
+      const balance = getBalance(customer.statement);
+
+      return response.json(balance);
+})
+
+app.listen(3333, () => console.log("Server is running!"));
